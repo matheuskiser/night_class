f3631047bcfb00d4bc9f0481e9cccb13e70f8b40
Matheus Iser <matheuskiser@gmail.com>
2015-01-29 19:58:26 -0800

    Added final solution

--- code_challenge_three.py
+++ code_challenge_three.py
@@ -1,25 +1,18 @@
 #####################################################################################################
 # Write a function to decode the following.
-
-str = "Yms bgb gr! Cmlepyrsjyrgmlq! Dgb wms sqc qrpgle.kyicrpylq? Id wms bgbl'r wms qfmsjb npmzyzjw em afcai gr msr. Ir kyicq rfgq ksaf cyqgcp."
-
 # A hint for the 3.
 # a = c
 # z = b
 # m = o
 
-shift   = 2
-letters = list(str)
-cipher  = ''
+# Used tip to use string.maketrans() from instructor
 
-ceasar = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+from string import maketrans   # Required to call maketrans function.
 
-for letter in letters:
-    if letter in ceasar:
-        oldindex = ceasar.index(letter)
-        newindex = (oldindex + shift) % len(ceasar)
-        newletter = ceasar[newindex]
-    else:
-        newletter = letter
+str = "Yms bgb gr! Cmlepyrsjyrgmlq! Dgb wms sqc qrpgle.kyicrpylq? Id wms bgbl'r wms qfmsjb npmzyzjw em afcai gr msr. Ir kyicq rfgq ksaf cyqgcp."
 
-    print(letter)
+intab = "abcdefghijklmnopqrstuvwxyz"
+outtab = "cdefghijklmnopqrstuvwxyzab"
+trantab = maketrans(intab, outtab)
+
+print str.translate(trantab);
